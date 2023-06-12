113539909ffa14d35c2d903ae69f0d83f798946b
youjin kim <gracecancho@gmail.com>
2023-06-12 14:00:27 +0900

    init

--- Programmers-Lv0/커피심부름.js
+++
@@ -1,7 +0,0 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/181837
-
-function solution(order) {
-    let money = 0;
-    order.forEach(o => o.includes("latte") ? money += 5000 : money += 4500)
-    return money;
-}
