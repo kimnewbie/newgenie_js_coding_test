1811bc81df336ad1ecf9a03ab8426e101e068c4c
youjin kim <gracecancho@gmail.com>
2023-06-13 13:57:05 +0900

    문자열 돌리기

---
+++ Programmers-Lv0/문자열돌리기.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close', function () {
+    str = input[0];
+    for (let i in str) {
+        console.log(str[i])
+    }
+    // or
+    // for (let i of str) {
+    //     console.log(i)
+    // }
+})
