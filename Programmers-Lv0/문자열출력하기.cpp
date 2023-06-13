eee2ded90429d30d8ac47c6bab603ce6ed8461ff
youjin kim <gracecancho@gmail.com>
2023-06-13 09:42:09 +0900

    문자열 출력하기

---
+++ Programmers-Lv0/문자열출력하기.cpp
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+
+// #include <stdio.h>
+// #define LEN_INPUT 1000001
+
+// int main(void)
+// {
+//     char s1[LEN_INPUT];
+//     scanf("%s", s1);
+//     printf("%s", s1);
+
+//     return 0;
+// }
