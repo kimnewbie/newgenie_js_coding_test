61e7c1e748a39f61e250560cc3f824c569410b23
youjin kim <gracecancho@gmail.com>
2023-06-20 09:18:12 +0900

    수열과 구간 쿼리 4

---
+++ 코딩 기초 트레이닝/Programmers-Lv0/수열과구간쿼리4.js
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181922
+
+function solution(arr, queries) {
+    for (let [s, e, k] of queries) {
+        for (let i = s; i <= e; i++) {
+            if (i % k === 0) arr[i]++;
+        }
+    }
+    return arr;
+}
