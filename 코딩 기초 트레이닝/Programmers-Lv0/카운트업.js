fcb6ac641730b8823a2a4b7799574826ab32f468
youjin kim <gracecancho@gmail.com>
2023-06-20 11:07:16 +0900

    카운트 업

---
+++ 코딩 기초 트레이닝/Programmers-Lv0/카운트업.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181920
+
+function solution(start, end) {
+    const answer = [];
+    for (let i = start; i <= end; i++) {
+        answer.push(i);
+    }
+    return answer;
+}
