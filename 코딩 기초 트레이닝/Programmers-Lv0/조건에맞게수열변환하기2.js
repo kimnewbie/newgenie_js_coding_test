4151599e63807c9e45a15b239653a7ebfc8f6307
youjin kim <gracecancho@gmail.com>
2023-06-22 11:29:42 +0900

    조건에 맞게 수열 변환하기 2

--- 코딩 기초 트레이닝/Programmers-Lv0/조건에맞게수열변환하기2.js
+++ 코딩 기초 트레이닝/Programmers-Lv0/조건에맞게수열변환하기2.js
@@ -1,2 +1,13 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181881
 
+function solution(arr, cnt = 0) {
+    while (!arr.every(x => (x >= 50 && x % 2 === 1) || (x < 50 && x % 2 === 0))) {
+        arr = arr.map(v => {
+            if (v >= 50 && v % 2 === 0) return v / 2;
+            if (v < 50 && v % 2 === 1) return v * 2 + 1;
+            return v;
+        })
+        cnt++;
+    }
+    return cnt;
+}  
