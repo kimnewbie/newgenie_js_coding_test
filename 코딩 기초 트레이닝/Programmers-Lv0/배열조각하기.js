319efc7a778aa980d1309ef0b7c7631456baae56
youjin kim <gracecancho@gmail.com>
2023-06-22 16:57:41 +0900

    배열 조작하기

---
+++ 코딩 기초 트레이닝/Programmers-Lv0/배열조각하기.js
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181893
+
+function solution(arr, query) {
+    for (let i = 0; i < query.length; i++) {
+        const cur = query[i]
+
+        if (i % 2 === 0) {
+            arr.splice(cur + 1, arr.length - (cur + 1))
+        } else {
+            arr.splice(0, cur)
+        }
+    }
+
+    return arr
+}
