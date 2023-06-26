75cedbeae82b0ce1595c1d9849d57eb97265b8c4
youjin kim <gracecancho@gmail.com>
2023-06-26 15:04:15 +0900

    간단한 식 계산하기

--- 코딩 기초 트레이닝/Programmers-Lv0/간단한식계산하기.js
+++ 코딩 기초 트레이닝/Programmers-Lv0/간단한식계산하기.js
@@ -1,6 +1,20 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/181865
+// eval() 함수는 절대 사용하지말라고 함
 
 function solution(binomial) {
     const [a, op, b] = binomial.split(' ');
     return eval(Number(a) + op + Number(b))
+}
+
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ');
+
+    switch (op) {
+        case "+":
+            return +a + +b;
+        case "-":
+            return +a - +b;
+        case "*":
+            return +a * +b;
+    }
 }
