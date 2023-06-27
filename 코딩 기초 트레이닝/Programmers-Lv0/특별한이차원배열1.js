0bfd5a3c42bd9498d31f9151fd0d0b55fa2c51ec
youjin kim <gracecancho@gmail.com>
2023-06-27 15:44:02 +0900

    특별한 이차원 배열 1

---
+++ 코딩 기초 트레이닝/Programmers-Lv0/특별한이차원배열1.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    const arr = Array.from(Array(n), () => Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j) arr[i][j] = 1
+            else arr[i][j] = 0
+        }
+    }
+    return arr;
+}
