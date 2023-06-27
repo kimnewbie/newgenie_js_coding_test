a62533e76538485130b0679b394e8161c995f0d1
youjin kim <gracecancho@gmail.com>
2023-06-27 16:25:46 +0900

    배열의 길이를 2의 거듭제곱으로 만들기

---
+++ 코딩 기초 트레이닝/Programmers-Lv0/배열의길이를2의거듭제곱으로만들기.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181857  
+
+function solution(arr) {
+    return [...arr, ...Array(2 ** Math.ceil(Math.log2(arr.length)) - arr.length).fill(0)];
+}
